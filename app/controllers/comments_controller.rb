07fd88dc35311330afdf889a3e3d3aa5a5c58622
yutoogi <yuto.ogi@fourier.jp>
2017-08-09 20:12:14 +0900

    Redirect fix for commented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.idea, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
